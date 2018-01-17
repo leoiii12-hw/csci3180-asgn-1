d47d8874a25efaa9ce3928ef1c77a29f9699ef04
Leo Choi <choimankin@gmail.com>
2018-01-17 22:21:22 +0800

    1. Initialised

---
+++ cobol/main.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Choi Man Kin
+      * Date: 17 Jan 2018
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
