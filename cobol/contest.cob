3d707d868f925f6b648ab2134dc051297945425f
Leo Choi <choimankin@gmail.com>
2018-02-02 00:28:02 +0800

    1. Modified Cobol version to read only once

--- cobol/contest.cob
+++ cobol/contest.cob
@@ -101,20 +101,25 @@
             04 SR-PROBLEM-ID PIC 9(1).
             04 SR-OUTCOME PIC X(19).
             04 SR-SCORE PIC 9(3).
-      
-       01 WS-PROCESSING-PROBLEM-ID PIC 9(2).
+       01 WS-SUBMISSION-RECORD-PROCESSED PIC 9(1).
+       01 WS-SUBMISSION-RECORD-ENDED PIC 9(1). 
 
-       01 WS-PROBLEM-MIN-SCORE PIC 9(3).
-       01 WS-PROBLEM-MAX-SCORE PIC 9(3).
-       01 WS-PROBLEM-BASE-SCORE PIC 9(3).
-       01 WS-PROBLEM-NUM-OF-SUBMISSIONS PIC 9(3).
-       01 WS-PROBLEM-TOTAL-SCORE PIC 9(3).
+       01 WS-TEAM-TABLE. 
+            05 WS-PROBLEM OCCURS 10 TIMES INDEXED BY I.
+                  10 WS-PROBLEM-MIN-SCORE PIC 9(3).
+                  10 WS-PROBLEM-MAX-SCORE PIC 9(3).
+                  10 WS-PROBLEM-BASE-SCORE PIC 9(3).
+                  10 WS-PROBLEM-NUM-OF-SUBMISSIONS PIC 9(3).
+                  10 WS-PROBLEM-TOTAL-SCORE PIC 9(3).
+
+       01 WS-PROCESSING-PROBLEM-ID PIC 9(2). 
+       01 WS-PROCESSING-I PIC 9(2). 
 
        01 WS-PROBLEM-B-SCORE PIC 9(3)V9(2).
        01 WS-PROBLEM-DECAY PIC 9(3)V9(2).
-       01 WS-PROBLEM-R-SCORE PIC 9(3)V9(2).
-
+       01 WS-PROBLEM-R-SCORE PIC 9(3)V9(2).   
        01 WS-PROBLEM-FINAL-SCORE PIC 9(3).
+       
        01 WS-ALL-PROBLEMS-SCORE PIC 9(4).
 
        01 ONE_NUMBER_STRING PIC 9(1).
@@ -129,6 +134,9 @@
 
             OPEN INPUT T-FILE.
             OPEN INPUT SR-FILE.
+
+            *> The first record is empty
+            MOVE 1 TO WS-SUBMISSION-RECORD-PROCESSED.
             
             GO TO TEAM-PROC.
        CREATE-OUTPUT-PROC.
@@ -136,22 +144,70 @@
             CLOSE NEW-FILE.
        RESET-ALL-VARIABLES-PROC.
             *> DISPLAY "RESET-ALL-VARIABLES-PROC".
+
             MOVE 0 TO WS-PROCESSING-PROBLEM-ID.
 
-            PERFORM RESET-PROBLEM-VARIABLES-PROC.
+            PERFORM RESET-TEAM-VARIABLES-PROC.
 
             MOVE 0 TO WS-PROBLEM-FINAL-SCORE.
             MOVE 0 TO WS-ALL-PROBLEMS-SCORE.
-       RESET-PROBLEM-VARIABLES-PROC.
-            MOVE 100 TO WS-PROBLEM-MIN-SCORE.
-            MOVE 0 TO WS-PROBLEM-MAX-SCORE.
-            MOVE 0 TO WS-PROBLEM-BASE-SCORE.
-            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS.
-            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE.
+       RESET-TEAM-VARIABLES-PROC.
+            SET I TO 1.
 
-            MOVE 0 TO WS-PROBLEM-B-SCORE.
-            MOVE 0 TO WS-PROBLEM-DECAY.
-            MOVE 0 TO WS-PROBLEM-R-SCORE.
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(1).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(2).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(3).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(4).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(5).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(6).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(7).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(8).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(9).
+            MOVE 100 TO WS-PROBLEM-MIN-SCORE(10).
+
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(1).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(2).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(3).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(4).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(5).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(6).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(7).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(8).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(9).
+            MOVE 0 TO WS-PROBLEM-MAX-SCORE(10).
+            
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(1).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(2).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(3).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(4).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(5).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(6).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(7).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(8).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(9).
+            MOVE 0 TO WS-PROBLEM-BASE-SCORE(10).
+
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(1).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(2).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(3).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(4).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(5).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(6).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(7).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(8).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(9).
+            MOVE 0 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(10).
+
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(1).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(2).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(3).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(4).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(5).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(6).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(7).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(8).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(9).
+            MOVE 0 TO WS-PROBLEM-TOTAL-SCORE(10).
        END-PROC.
             *> DISPLAY "END-PROC".
 
@@ -170,116 +226,130 @@
 
             PERFORM DISPLAY-TEAM-NAME-PROC.
 
-            GO TO SCAN-RECORDS-PROC.
-       SCAN-RECORDS-PROC.
-            *> DISPLAY "SCAN-RECORDS-PROC".
-      
-            *> RESET SR-FILE
-            CLOSE SR-FILE.
-            OPEN INPUT SR-FILE.
-
-            PERFORM DISPLAY-PROBLEM-ID-PROC.
-
-            PERFORM RESET-PROBLEM-VARIABLES-PROC.
+            PERFORM RESET-TEAM-VARIABLES-PROC.
 
             GO TO SCAN-RECORDS-LOOP-PROC.
        SCAN-RECORDS-LOOP-PROC.
             *> DISPLAY "SCAN-RECORDS-LOOP-PROC".
-       
-            READ SR-FILE INTO WS-SUBMISSION-RECORD
-                  AT END GO TO PROBLEM-POST-PROC
-            END-READ.
+
+            IF WS-SUBMISSION-RECORD-PROCESSED = 0 THEN
+                  IF T-TEAM-NAME = SR-TEAM-NAME THEN
+                        PERFORM SCAN-RECORDS-ACTION-PROC
+                        MOVE 1 TO WS-SUBMISSION-RECORD-PROCESSED
+                  END-IF
+            END-IF.
+
+            IF WS-SUBMISSION-RECORD-PROCESSED = 1 THEN
+                  IF WS-SUBMISSION-RECORD-ENDED <> 1 THEN
+                        READ SR-FILE INTO WS-SUBMISSION-RECORD
+                              AT END
+                                    MOVE 1 TO WS-SUBMISSION-RECORD-ENDED
+                                    GO TO CAL-PROBLEM-SCORE-LOOP-PROC
+                        END-READ
+                  END-IF
+                  MOVE 0 TO WS-SUBMISSION-RECORD-PROCESSED
+            END-IF.
 
             IF T-TEAM-NAME = SR-TEAM-NAME THEN
-                  IF SR-PROBLEM-ID = WS-PROCESSING-PROBLEM-ID THEN
-                        PERFORM SCAN-RECORDS-ACTION-PROC
-                  END-IF
+                  PERFORM SCAN-RECORDS-ACTION-PROC
+                  MOVE 1 TO WS-SUBMISSION-RECORD-PROCESSED
+            END-IF.
+
+            IF T-TEAM-NAME <> SR-TEAM-NAME THEN
+                  GO TO CAL-PROBLEM-SCORE-LOOP-PROC
             END-IF.
 
             GO TO SCAN-RECORDS-LOOP-PROC.
        SCAN-RECORDS-ACTION-PROC.
             *> DISPLAY "SCAN-RECORDS-ACTION-PROC".
 
-            MOVE SR-SCORE TO WS-PROBLEM-BASE-SCORE.            
-            IF SR-SCORE < WS-PROBLEM-MIN-SCORE THEN
-                  MOVE SR-SCORE TO WS-PROBLEM-MIN-SCORE
+            COMPUTE WS-PROCESSING-I = SR-PROBLEM-ID + 1.
+            SET I TO WS-PROCESSING-I.
+
+            MOVE SR-SCORE TO WS-PROBLEM-BASE-SCORE(I).            
+            IF SR-SCORE < WS-PROBLEM-MIN-SCORE(I) THEN
+                  MOVE SR-SCORE TO WS-PROBLEM-MIN-SCORE(I)
             END-IF.
-            IF SR-SCORE > WS-PROBLEM-MAX-SCORE THEN
-                  MOVE SR-SCORE TO WS-PROBLEM-MAX-SCORE
+            IF SR-SCORE > WS-PROBLEM-MAX-SCORE(I) THEN
+                  MOVE SR-SCORE TO WS-PROBLEM-MAX-SCORE(I)
             END-IF.
 
-            ADD 1 TO WS-PROBLEM-NUM-OF-SUBMISSIONS
-                  GIVING WS-PROBLEM-NUM-OF-SUBMISSIONS.
+            ADD 1 TO WS-PROBLEM-NUM-OF-SUBMISSIONS(I)
+                  GIVING WS-PROBLEM-NUM-OF-SUBMISSIONS(I).
 
-            ADD SR-SCORE TO WS-PROBLEM-TOTAL-SCORE
-                  GIVING WS-PROBLEM-TOTAL-SCORE.
-
+            ADD SR-SCORE TO WS-PROBLEM-TOTAL-SCORE(I)
+                  GIVING WS-PROBLEM-TOTAL-SCORE(I).
+            
             *> DISPLAY
             *>       WS-PROBLEM-BASE-SCORE, " ",
             *>       WS-PROBLEM-NUM-OF-SUBMISSIONS, " ",
             *>       WS-PROBLEM-TOTAL-SCORE.
-       PROBLEM-POST-PROC.
-            *> DISPLAY "PROBLEM-POST-PROC".
+       CAL-PROBLEM-SCORE-LOOP-PROC.
+            *> DISPLAY "CAL-PROBLEM-SCORE-LOOP-PROC".
 
-            ADD 1 TO WS-PROCESSING-PROBLEM-ID
-                  GIVING WS-PROCESSING-PROBLEM-ID.
-
-            PERFORM PROBLEM-SCORE-PRINT-PROC.
+            *> DISPLAY
+            *>       "*",
+            *>       WS-PROBLEM-BASE-SCORE(1), " ",
+            *>       WS-PROBLEM-NUM-OF-SUBMISSIONS(1), " ",
+            *>       WS-PROBLEM-TOTAL-SCORE(1), " ",
+            *>       WS-PROBLEM-MAX-SCORE(1), " "
+            *>       WS-PROBLEM-MIN-SCORE(1), " "
+            *>       WS-PROBLEM-FINAL-SCORE,
+            *>       "*" NO ADVANCING.
 
             IF WS-PROCESSING-PROBLEM-ID = 10 THEN
                   PERFORM DISPLAY-TEAM-SCORE-PROC
                   GO TO TEAM-PROC
             END-IF.
 
-            GO TO SCAN-RECORDS-PROC.
-       PROBLEM-SCORE-PRINT-PROC.
-            *> DISPLAY "PROBLEM-SCORE-PRINT-PROC".
+            MOVE 0 TO WS-PROBLEM-B-SCORE.
+            MOVE 0 TO WS-PROBLEM-DECAY.
+            MOVE 0 TO WS-PROBLEM-R-SCORE.
 
-            MOVE 0 TO WS-PROBLEM-FINAL-SCORE.
+            MOVE 0 TO WS-PROBLEM-FINAL-SCORE.            
             
-            *> DISPLAY
-            *>       "*",
-            *>       WS-PROBLEM-BASE-SCORE, " ",
-            *>       WS-PROBLEM-NUM-OF-SUBMISSIONS, " ",
-            *>       WS-PROBLEM-TOTAL-SCORE, " ",
-            *>       WS-PROBLEM-MAX-SCORE, " "
-            *>       WS-PROBLEM-MIN-SCORE, " "
-            *>       WS-PROBLEM-FINAL-SCORE,
-            *>       "*" NO ADVANCING.
+            COMPUTE WS-PROCESSING-I = WS-PROCESSING-PROBLEM-ID + 1.
+            SET I TO WS-PROCESSING-I.
 
-            IF (WS-PROBLEM-MAX-SCORE > 0) THEN
-                  MOVE WS-PROBLEM-BASE-SCORE TO WS-PROBLEM-B-SCORE
+            IF (WS-PROBLEM-MAX-SCORE(I) > 0) THEN
+                  MOVE WS-PROBLEM-BASE-SCORE(I) TO WS-PROBLEM-B-SCORE
 
-                  IF (WS-PROBLEM-BASE-SCORE = 100) THEN
+                  IF (WS-PROBLEM-BASE-SCORE(I) = 100) THEN
                         MOVE 1.0 TO WS-PROBLEM-DECAY
                   END-IF
-                  IF (WS-PROBLEM-BASE-SCORE < 100) THEN
+                  IF (WS-PROBLEM-BASE-SCORE(I) < 100) THEN
                         COMPUTE WS-PROBLEM-DECAY =
-                              1.0 / WS-PROBLEM-NUM-OF-SUBMISSIONS
+                              1.0 / WS-PROBLEM-NUM-OF-SUBMISSIONS(I)
                   END-IF
 
-                  IF (WS-PROBLEM-MAX-SCORE <= 30) THEN
+                  IF (WS-PROBLEM-MAX-SCORE(I) <= 30) THEN
                         MOVE 0 TO WS-PROBLEM-R-SCORE
                   END-IF
-                  IF (WS-PROBLEM-MAX-SCORE > 30) THEN
+                  IF (WS-PROBLEM-MAX-SCORE(I) > 30) THEN
                         COMPUTE WS-PROBLEM-R-SCORE =
                               100 -
-                              WS-PROBLEM-MAX-SCORE +
-                              WS-PROBLEM-MIN-SCORE
+                              WS-PROBLEM-MAX-SCORE(I) +
+                              WS-PROBLEM-MIN-SCORE(I)
                   END-IF
 
                   COMPUTE WS-PROBLEM-FINAL-SCORE =
                         0.6*WS-PROBLEM-B-SCORE*WS-PROBLEM-DECAY+
-                        0.3*WS-PROBLEM-TOTAL-SCORE/
-                        WS-PROBLEM-NUM-OF-SUBMISSIONS+
+                        0.3*WS-PROBLEM-TOTAL-SCORE(I)/
+                        WS-PROBLEM-NUM-OF-SUBMISSIONS(I)+
                         0.1*WS-PROBLEM-R-SCORE
             END-IF.
 
-            PERFORM DISPLAY-PROBLEM-SCORE-PROC.            
+            PERFORM DISPLAY-PROBLEM-ID-PROC.
+            PERFORM DISPLAY-PROBLEM-SCORE-PROC.
 
             *> ADD TO TOTAL
             ADD WS-PROBLEM-FINAL-SCORE TO WS-ALL-PROBLEMS-SCORE
                   GIVING WS-ALL-PROBLEMS-SCORE.
+
+            ADD 1 TO WS-PROCESSING-PROBLEM-ID
+                  GIVING WS-PROCESSING-PROBLEM-ID.
+
+            GO TO CAL-PROBLEM-SCORE-LOOP-PROC.
        DISPLAY-HEADER-PROC.
             OPEN EXTEND CONTEST-HEADER-FILE.
             MOVE "2018 CUHK CSE Programming Contest" TO CF-HEADER-DATA
