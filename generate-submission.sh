832013b32198cfc3ccdd8a05f539235db203f1f3
Leo Choi <choimankin@gmail.com>
2018-02-03 18:39:24 +0800

    1. Update submission

--- generate-submission.sh
+++ generate-submission.sh
@@ -3,3 +3,5 @@
 mkdir -p submission
 cp cobol/contest.cob submission/contest.cob
 cp fortran/contest.for submission/contest.for
+cp report.pdf submission/report.pdf
+cp receipt.pdf submission/receipt.pdf
